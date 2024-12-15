3731b614f72e3d13c72d04da5055cd6417804455
VictosVertex <Victos.Vertex@gmail.com>
2024-12-15 20:38:05 +0100

    Let literals track their text. Also update comments.

--- src/mkdocstrings_handlers/asp/semantics/literal.py
+++ src/mkdocstrings_handlers/asp/semantics/literal.py
@@ -7,14 +7,31 @@ from tree_sitter import Node
 
 @dataclass
 class Literal:
+    """A literal in an ASP document."""
+
     identifier: str
+    """ The identifier of the literal. """
     arity: int
+    """ The arity (number of parameters) of the literal. """
     negation: bool
+    """ Whether the literal is negated. """
+    text: str
+    """ Plain ASP text of the literal. """
 
     @staticmethod
     def from_node(node: Node) -> Literal:
-        # If the literal has a single child, it is positive
+        """
+        Create a literal from a node.
 
+        Args:
+            node: The node representing the literal.
+
+        Returns:
+            The created literal.
+        """
+        text = node.text.decode("utf-8")
+
+        # If the literal has a single child or is double-negated, it is positive
         negation = node.child_count > 1 and node.children[0].child_count == 1
 
         atom = node.children[0] if node.child_count == 1 else node.children[1]
@@ -27,4 +44,4 @@ class Literal:
             terms = atom.children[1].children[0]
             arity = len(terms.children) // 2
 
-        return Literal(identifier, arity, negation)
+        return Literal(identifier, arity, negation, text)

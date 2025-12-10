28de33e41bc0afc57a2687fd068d2d0f2840fa31
VictosVertex <Victos.Vertex@gmail.com>
2025-12-10 09:52:17 +0100

    add tests for debug

--- src/mkdocstrings_handlers/asp/_internal/collect/debug.py
+++ src/mkdocstrings_handlers/asp/_internal/collect/debug.py
@@ -10,10 +10,8 @@ def print_tree(node: Node, source: bytes, depth: int) -> None:
         source: The source code from which the node was created.
         depth: The current depth in the tree.
     """
-    if node.text is None:
-        raise ValueError(f"Text of node {node} is None.")
 
-    node_text = node.text.decode("utf-8")
+    node_text = node.text.decode("utf-8") if node.text is not None else ""
 
     print(f"{'  ' * depth}{node.grammar_name} {node.id}: {node_text}")
 
